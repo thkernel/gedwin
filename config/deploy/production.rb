35abb7ed120ebe16ee524dcdecd24c02bbcb9cde
thkernel <a.dembele@upcase.net>
2020-07-23 08:16:25 +0000

    update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,11 +7,11 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :application, "gedwin.prod"               # the name of your website - should also be the name of the directory
-set :deploy_to, "/home/ubuntu/rails-apps/gedwin/gedwin.prod"   # the path to your new deployment directory on the server - by default, the name of the application (e.g. "/var/www/sites/example.com")
+set :deploy_to, "/home/ubuntu/apps/rails/gedwin/gedwin.prod"   # the path to your new deployment directory on the server - by default, the name of the application (e.g. "/var/www/sites/example.com")
 set :rails_env, "production"
 
 #server "51.38.235.131", user: "root", roles: %w{app db web}, ssh_options: {forward_agent: true}
-server "51.38.235.131", user: "root", roles: %w{app db web}, ssh_options: {forward_agent: true}
+server "51.89.167.78", user: "ubuntu", roles: %w{app db web}, ssh_options: {forward_agent: true}
 
 set :branch, "master"
 set :port, 3030
