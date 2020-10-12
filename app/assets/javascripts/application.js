d4d23a0dcb8e8434f532fd5a794834c72d87b2cc
thkernel <a.dembele@upcase.net>
2020-10-12 11:37:59 +0000

    fix js import bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -53,8 +53,8 @@
 //= require lib/jquery.flot/jquery.flot
 //= require lib/jquery.flot/jquery.flot.resize
 //= require lib/flot-spline/js/jquery.flot.spline.min
-//= require lib/jquery-sparkline/jquery.sparkline.min
-//= require lib/echarts/echarts.min
+// require lib/jquery-sparkline/jquery.sparkline.min
+// require lib/echarts/echarts.min
 //= require lib/select2/js/select2.full.min
 
 //= require js/bracket
